d73cb6b1b6b4342a4edd4c979db299e7ae8a7968
Hymavathi1929 <24pa1a05c5@vishnu.edu.in>
2025-08-09 10:35:50 +0530

    Create Factorial.c

---
+++ Factorial.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+     int sum=1;
+     printf("enter the number");
+     scanf("%d",&n);
+     for(int i=1;i<=n;i++)
+            sum=sum*i;
+     printf("the factorial of %d number",n,sum);
+}
